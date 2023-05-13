e14c3fff0cd2ff335f8620ef304f1f856413b33c
Kibou <valdezcleo28@gmail.com>
2023-05-13 09:22:20 -0700

    response comment at MainActivity.java

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // hi cleo, kamusta kau ken G? hehe
+        //haan kam agsarsaritan ngem mamiss ko latta suna.....nuh dadduma. Sikayo ngay?
 
 
     }
