98bd1ede60f350b610f3cf0ef8b1e3ec6e67c36c
Kibou <valdezcleo28@gmail.com>
2023-05-28 11:06:39 +0800

    VsPlayer initial layout commit

--- app/src/main/java/com/example/tictactoe/VsPlayer/AddPlayers.java
+++ app/src/main/java/com/example/tictactoe/VsPlayer/AddPlayers.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.tictactoe.Home;
 import com.example.tictactoe.R;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddPlayers extends AppCompatActivity {
 
     TextInputEditText playerOne, playerTwo;
-    Button startBtn;
+    Button startBtn, backBtn;
+    String getPlayerOne, getPlayerTwo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,41 @@ public class AddPlayers extends AppCompatActivity {
         playerOne = findViewById(R.id.plyrOne);
         playerTwo = findViewById(R.id.plyrTwo);
         startBtn = findViewById(R.id.startBtn);
+        backBtn = findViewById(R.id.backBtn);
 
-        playerOne.getText().toString();
-        playerTwo.getText().toString();
 
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPlayerOne = playerOne.getText().toString();
+                getPlayerTwo = playerTwo.getText().toString();
+
+                if(getPlayerOne.isEmpty() && getPlayerTwo.isEmpty()){
+                    Toast.makeText(AddPlayers.this, "Please enter your names", Toast.LENGTH_SHORT).show();
+                } else {
+                    openMulti();
+                }
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
     }
 
     public void openMulti(){
-        Intent intent = new Intent();
+        Intent intent = new Intent(getApplicationContext(), VsPlayer.class);
+        intent.putExtra("playerone", getPlayerOne);
+        intent.putExtra("playertwo", getPlayerTwo);
+        startActivity(intent);
+        finish();
+    }
+    public void openHome(){
+        Intent intent = new Intent(getApplicationContext(), Home.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/tictactoe/VsPlayer/VsPlayer.java
@@ -0,0 +1,17 @@
+package com.example.tictactoe.VsPlayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tictactoe.R;
+
+public class VsPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vs_player);
+
+    }
+}
